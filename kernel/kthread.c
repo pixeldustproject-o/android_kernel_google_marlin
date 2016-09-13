b133607b22ec3b3d889cbe30602b93b9dc362d22
Andy Lutomirski <luto@kernel.org>
2016-09-13 14:29:24 -0700

    sched/core: Allow putting thread_info into task_struct
    Squashed with:
    
    benzo_defconfig: Enable thread info in task
    perf: get cpu value using task_cpu(tsk)
    kernel/fork: Fix build break in fork.c when THREAD_SIZE < PAGE_SIZE
    locking: avoid passing around 'thread_info' in mutex debugging code
    kdb: use task_cpu() instead of task_thread_info()->cpu
    arm64: restore get_current() optimisation
    arm64: split thread_info from task stack
    arm64: Store struct thread_info in sp_el0
    arm64: assembler: introduce ldr_this_cpu
    arm64: smp: Prevent raw_smp_processor_id() recursion
    arm64: make cpu number a percpu variable
    arm64: smp: prepare for smp_processor_id() rework
    arm64: move sp_el0 and tpidr_el1 into cpu_suspend_ctx
    arm64: prep stack walkers for THREAD_INFO_IN_TASK
    arm64: unexport walk_stackframe
    arm64: traps: simplify die() and __die()
    arm64: factor out current_stack_pointer
    arm64: asm-offsets: remove unused definitions
    arm64: thread_info remove stale items
    thread_info: include <current.h> for THREAD_INFO_IN_TASK
    thread_info: factor out restart_block
    kthread: Pin the stack via try_get_task_stack()/put_task_stack() in to_live_kthread() function
    sched/core: Add try_get_task_stack() and put_task_stack()
    sched/core: Allow putting thread_info into task_struct
    printk: when dumping regs, show the stack, not thread_info
    kernel: fix up initial thread stack pointer vs thread_info confusion
    kernel: Clarify naming of thread info/stack allocators
    
    Change-Id: I815ebe48c2d87ab5c3840b14a6e52573794edb29
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -64,7 +64,7 @@ static inline struct kthread *to_kthread(struct task_struct *k)
 static struct kthread *to_live_kthread(struct task_struct *k)
 {
 	struct completion *vfork = ACCESS_ONCE(k->vfork_done);
-	if (likely(vfork))
+	if (likely(vfork) && try_get_task_stack(k))
 		return __to_kthread(vfork);
 	return NULL;
 }
@@ -424,8 +424,10 @@ void kthread_unpark(struct task_struct *k)
 {
 	struct kthread *kthread = to_live_kthread(k);
 
-	if (kthread)
+	if (kthread) {
 		__kthread_unpark(k, kthread);
+		put_task_stack(k);
+	}
 }
 
 /**
@@ -453,6 +455,7 @@ int kthread_park(struct task_struct *k)
 				wait_for_completion(&kthread->parked);
 			}
 		}
+		put_task_stack(k);
 		ret = 0;
 	}
 	return ret;
@@ -487,6 +490,7 @@ int kthread_stop(struct task_struct *k)
 		__kthread_unpark(k, kthread);
 		wake_up_process(k);
 		wait_for_completion(&kthread->exited);
+		put_task_stack(k);
 	}
 	ret = k->exit_code;
 	put_task_struct(k);
