aef1af957b9bbf5642b604f6d4b2f8bcfc296b54
flar2 <asegaert@gmail.com>
2016-10-24 22:41:17 -0400

    touch: fix enable wakeup gesture while tp is suspended
    Make sure setting isn't applied until next screen on, to prevent unbalanced irq_wake
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- drivers/input/touchscreen/synaptics_dsx_htc_2.6/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_htc_2.6/synaptics_dsx_core.c
@@ -147,6 +147,9 @@ bool scr_suspended(void)
 }
 #endif
 
+static bool wakeup_gesture_changed = false;
+static bool wakeup_gesture_temp;
+
 static int synaptics_rmi4_check_status(struct synaptics_rmi4_data *rmi4_data,
 		bool *was_in_bl_mode);
 static int synaptics_rmi4_free_fingers(struct synaptics_rmi4_data *rmi4_data);
@@ -1102,8 +1105,14 @@ static ssize_t synaptics_rmi4_wake_gesture_store(struct device *dev,
 
 	input = input > 0 ? 1 : 0;
 
-	if (rmi4_data->f11_wakeup_gesture || rmi4_data->f12_wakeup_gesture)
-		rmi4_data->enable_wakeup_gesture = input;
+	if (rmi4_data->f11_wakeup_gesture || rmi4_data->f12_wakeup_gesture) {
+		if (rmi4_data->suspend) { 
+			wakeup_gesture_changed = true;
+			wakeup_gesture_temp = input;
+		} else {
+			rmi4_data->enable_wakeup_gesture = input;
+		}
+	}
 
 	return count;
 }
