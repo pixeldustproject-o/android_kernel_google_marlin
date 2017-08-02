c23640cb8d1f87952074c1cf1ee15a6ba29b98ca
Patrick Bellasi <patrick.bellasi@arm.com>
2017-08-02 15:41:29 +0100

    sched/fair: add stats for new find_best_target cases
    This adds counters for some additional fbt() cases:
     - IDLE CPUs selected while in low utilization mode
     - best active CPU for prefer_idle tasks
     - best idle CPU for !prefer_idle tasks
    
    Change-Id: Ieb13357d2e69910b05cba59ec60a70dd3817a6e7
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1030,6 +1030,9 @@ struct eas_stats {
 	u64 fbt_no_cpu;
 	u64 fbt_no_sd;
 	u64 fbt_pref_idle;
+	u64 fbt_pref_idle_lum;
+	u64 fbt_best_active;
+	u64 fbt_best_idle;
 	u64 fbt_count;
 
 	/* cas */
@@ -1283,6 +1286,9 @@ struct sched_statistics {
 	u64			nr_wakeups_fbt_no_cpu;
 	u64			nr_wakeups_fbt_no_sd;
 	u64			nr_wakeups_fbt_pref_idle;
+	u64			nr_wakeups_fbt_pref_idle_lum;
+	u64			nr_wakeups_fbt_best_active;
+	u64			nr_wakeups_fbt_best_idle;
 	u64			nr_wakeups_fbt_count;
 
 	/* cas */
