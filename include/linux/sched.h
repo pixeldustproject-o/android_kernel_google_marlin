cb6f54b86342e40407d8bd59b6fce53f21736359
luca abeni <luca.abeni@santannapisa.it>
2017-09-07 12:09:31 +0200

    sched/deadline: Use C bitfields for the state flags
    Ask the compiler to use a single bit for storing true / false values,
    instead of wasting the size of a whole int value.
    Tested with gcc 5.4.0 on x86_64, and the compiler produces the expected
    Assembly (similar to the Assembly code generated when explicitly accessing
    the bits with bitmasks, "&" and "|").
    
    Change-Id: I8db61e478f35f299835c06df4adadb36dcb51e44
    Signed-off-by: luca abeni <luca.abeni@santannapisa.it>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Juri Lelli <juri.lelli@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1504778971-13573-5-git-send-email-luca.abeni@santannapisa.it
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1421,7 +1421,10 @@ struct sched_dl_entity {
 	 * @dl_yielded tells if task gave up the cpu before consuming
 	 * all its available runtime during the last job.
 	 */
-	int dl_throttled, dl_boosted, dl_yielded, dl_non_contending;
+	int				dl_throttled      : 1;
+	int				dl_boosted        : 1;
+	int				dl_yielded        : 1;
+	int				dl_non_contending : 1;
 
 	/*
 	 * Bandwidth enforcement timer. Each -deadline task has its
