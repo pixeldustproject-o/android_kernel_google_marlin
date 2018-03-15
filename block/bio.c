a68a3d4b85028a582ea2060bfda969ffe4de36f1
mydongistiny <jaysonedson@gmail.com>
2018-03-15 06:52:05 -0700

    Revert "block: cgroups: introduce the BFQ-v8r8 I/O sched"
    This reverts commit e572f876221a5acf1ee959dc9e54f3392bd4ea33.
    
    Change-Id: I1540f6c2f2c28b79412573a556d5a1ca92a78ab3
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- block/bio.c
+++ block/bio.c
@@ -31,12 +31,6 @@
 
 #include <trace/events/block.h>
 
-#include <linux/version.h>
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,2,0) /* BFQ backport fixup */
-#define bio_associate_blkcg(a, b) 0
-#endif
-
-
 /*
  * Test patch to inline a certain number of bi_io_vec's inside the bio
  * itself, to shrink a bio data allocation from two mempool calls to one
@@ -575,7 +569,6 @@ void __bio_clone_fast(struct bio *bio, struct bio *bio_src)
 	bio->bi_io_vec = bio_src->bi_io_vec;
 	bio->bi_dio_inode = bio_src->bi_dio_inode;
 	bio->bi_crypt_ctx = bio_src->bi_crypt_ctx;
-	bio_clone_blkcg_association(bio, bio_src);
 }
 EXPORT_SYMBOL(__bio_clone_fast);
 
@@ -680,8 +673,6 @@ integrity_clone:
 			return NULL;
 		}
 	}
-
-	bio_clone_blkcg_association(bio, bio_src);
 
 	return bio;
 }
@@ -2052,17 +2043,6 @@ void bio_disassociate_task(struct bio *bio)
 		css_put(bio->bi_css);
 		bio->bi_css = NULL;
 	}
-}
-
-/**
- * bio_clone_blkcg_association - clone blkcg association from src to dst bio
- * @dst: destination bio
- * @src: source bio
- */
-void bio_clone_blkcg_association(struct bio *dst, struct bio *src)
-{
-	if (src->bi_css)
-		WARN_ON(bio_associate_blkcg(dst, src->bi_css));
 }
 
 #endif /* CONFIG_BLK_CGROUP */
